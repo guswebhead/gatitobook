7185a4d31c0b9cda4ae21d725f77e82eee20d3be
gustavo.caixeta <gustavo.caixeta42@gmail.com>
2022-01-23 22:23:14 -0300

    implemtado a pagina de detalhe das fotos + novo interceptor e funções like e delete de fotos

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,27 +1,38 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import { catchError, mapTo, Observable, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+
 import { TokenService } from './../autenticacao/token.service';
-import { Animais } from './animais';
+import { Animais, Animal } from './animais';
 
 const API = environment.api
+const NOT_MODIFIED = '304';
 @Injectable({
   providedIn: 'root'
 })
 export class AnimaisService {
-
-
-
   constructor(
     private http: HttpClient,
     private tokenService: TokenService
   ) { }
 
   listUser(nameUser: string): Observable<Animais> {
-    const token = this.tokenService.getToken()
-    const headers = new HttpHeaders().append('x-access-token', token)
-    return this.http.get<Animais>(API + '/' + nameUser + '/photos', {headers})
+    return this.http.get<Animais>(API + '/' + nameUser + '/photos')
+  }
+  searchID(id: number): Observable<Animal> {
+    return this.http.get<Animal>(API + "/photos/" + id)
+  }
+  deleteAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(API + '/photos/' + id);
+  }
+
+  like(id: number): Observable<boolean> {
+    return this.http.post(API + '/photos/' + id + '/like', {}, { observe: 'response' }).pipe(
+      mapTo(true),
+      catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(() => new Error(error))
+      })
+    )
   }
 }
